048ca7e832343d1e9d9b6a2ef24b2a8897b0e9cf
miketambu <miketambu@gmail.com>
2010-11-15 15:54:51 +0000

    - DONE:  - Added contours2 tutorial

---
+++ tutorial/openCVtutorial/build/contours2/CMakeFiles/contours2.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/michele/workspace/openCVtutorial/src/contours2/contours2Main.cxx" "/home/michele/workspace/openCVtutorial/build/contours2/CMakeFiles/contours2.dir/contours2Main.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ tutorial/openCVtutorial/build/contours2/CMakeFiles/contours2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/contours2.dir/contours2Main.cxx.o"
+  "contours2.pdb"
+  "contours2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/contours2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
