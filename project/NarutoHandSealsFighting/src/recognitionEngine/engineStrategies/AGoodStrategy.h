bff38a3679aa00c1355b1c05a8890823e408d64b
miketambu <miketambu@gmail.com>
2011-01-04 14:00:40 +0000

    -DONE:   - strategies comments

--- project/NarutoHandSealsFighting/src/recognitionEngine/engineStrategies/AGoodStrategy.h
+++ project/NarutoHandSealsFighting/src/recognitionEngine/engineStrategies/AGoodStrategy.h
@@ -4,6 +4,17 @@
  * AGoodStrategy.h
  ******************************************************
  * DESCRIPTION:
+ * Engine Strategy
+ * It adds to the engine pool the following modules in
+ * order:
+ * 1. Sobel with pars: CV_SCHARR,1,0;
+ * 2. Blur with pars: CV_GAUSSIAN,3,3
+ * 3. Laplacian with kernel 7x7
+ * 4. Canny module with kernel 7x7 and threshold centered
+ * 		on the mean of the input image and wide 'range'
+ * 5. ContoursFinder with the mean of the input image as
+ * 		the threshold
+ * 6. Closure iterated for 15 cycles.
  *
  ******************************************************
  *	Created on: Jan 1, 2011
--- project/NarutoHandSealsFighting/src/recognitionEngine/engineStrategies/XperienceStrategy.h
+++ project/NarutoHandSealsFighting/src/recognitionEngine/engineStrategies/XperienceStrategy.h
@@ -4,6 +4,11 @@
  * XperienceStrategy.h
  ******************************************************
  * DESCRIPTION:
+ * Engine Strategy
+ * It adds the following modules:
+ * 1. Blurr with pars CV_GAUSSIAN,3,3
+ * 2. Laplacian with kernel size 7x7
+ * 3. Closure iterated for 15 iterations
  *
  ******************************************************
  *	Created on: Jan 2, 2011
